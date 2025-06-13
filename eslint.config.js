81208f3cb8852178c18117ba2d4f27cf7fde73ae
Atharv Bhosale <120968231+Atharv1136@users.noreply.github.com>
2025-06-13 18:00:35 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -21,8 +21,4 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  }
-);
+        
